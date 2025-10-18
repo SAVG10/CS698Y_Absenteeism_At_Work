256ff2d8845865b287b84a400024dbbbbbdb51d0
Rohan <rohanb21@iitk.ac.in>
2025-10-18 21:27:15 +0530

    Hosting with python anywhere

--- absenteeism_project/settings.py
+++ absenteeism_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ulek3xz04u3rp(fhk!3l+04bb^c1b(q6bs(a=kgrum&tm^vurl'
-
+# SECRET_KEY = 'django-insecure-ulek3xz04u3rp(fhk!3l+04bb^c1b(q6bs(a=kgrum&tm^vurl'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rohan22batra.pythonanywhere.com']
 
 
 # Application definition
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
